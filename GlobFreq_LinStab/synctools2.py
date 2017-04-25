tsim calculation in synctools interface fixed. first code for cherquerboard states in synctools

GlobFreq_LinStab/synctools2.py
@@ -120,7 +120,7 @@ class CouplingFunction(object):
 
 class Triangle(CouplingFunction):
     ''' Periodic triangle signal vertically centered around 0'''
-    def __init__(self, freq):
+    def __init__(self, freq=1.0 / (2 * np.pi)):
         self.freq = freq
 
     def __call__(self, t):
@@ -240,6 +240,12 @@ class Pll(object):
 # #############################################################################
 
 
+
+
+
+
+
+
 class PllSystem(object):
     def __init__(self, pll, graph):
         self.pll = pll
@@ -271,25 +277,74 @@ def wrap2pi(x):
     return np.mod(x + np.pi, 2 * np.pi) - np.pi
 
 
-class State(object):
-    def __init__(self, system):
+class SyncState(object):
+    def __init__(self, system, omega):
         self.sys = system
+        self.omega = omega
+
+    @staticmethod
+    def get_omega(system, ns=1000):
+        # Get coupling sum function
+        h_sum = SyncState.get_coupling_sum(system)
+
+        # Setup implicit equation for s
+        f = lambda s: system.g.k * system.g.tau * h_sum(s) + system.pll.w * system.g.tau - s
+
+        # Determine search interval for s
+        # Assumption c can only vary between -1 and +1
+        s_min = (system.pll.w - system.g.k) * system.g.tau
+        s_max = (system.pll.w + system.g.k) * system.g.tau
+        s = np.linspace(s_min - 2, s_max + 2, ns)   # safty margin added
+
+        # Find sign changes as you go along curve
+        # Assumes that there are no double sign changes between two values of s
+        # A finer sampling interval can be achieved by increasing ns
+        i_root = get_sign_changes(f(s))
+        if len(i_root) > 0:
+            omega = []
+            for ir in range(len(i_root)):
+                # Numerically solve the implicit equation for omega
+                s_tmp = optimize.brentq(f, s[i_root[ir]], s[i_root[ir] + 1])
+                omega.append(system.pll.w + system.g.k * h_sum(s_tmp))
+            return omega
+        else:
+            return None
+
+    @staticmethod
+    def get_states(sys):
+        raise NotImplementedError
+
+    def get_stability(self):
+        raise NotImplementedError
 
     @classmethod
-    def get_states(cls, sys):
+    def get_coupling_sum(cls, system, k=0):
+        dphi = cls.get_dphi(n, m)
+        dphi = dphi * (np.arange(n) - k)
+        s = np.asarray(s)
+        if s.size > 1:
+            c_sum = np.zeros(s.size)
+            for i in range(s.size):
+                c_sum[i] = np.sum(c * h(-s[i] + dphi))
+        else:
+            c_sum = np.sum(c * h(-s + dphi))
+        return c_sum
+
+    @staticmethod
+    def get_dphi(system):
         raise NotImplementedError
 
 
-class LinearState(State):
-    pass
 
 
-class Twist(LinearState):
+
+
+
+
+class Twist(SyncState):
     def __init__(self, system, m, omega):
-        super(Twist, self).__init__(system)
+        super(Twist, self).__init__(system, omega)
         self.m = m
-        self.omega = omega
-        self.sys = system
         n = self.sys.g.arr.get_n()
         self.dphi = Twist.get_dphi(n, m)
 
@@ -436,3 +491,268 @@ class Twist(LinearState):
             return omega
         else:
             return None
+
+
+
+
+# #############################################################################
+
+
+
+
+
+class SyncStateDefinition(object):
+    def __init__(self, system):
+        self.sys = system
+
+
+    def get_phi(self):
+        raise NotImplementedError
+
+
+    def get_dphi_matrix(self):
+        '''The static phase difference matrix
+
+           dphi[i, j] = phi[j] - phi[i]
+        '''
+        n = self.sys.g.arr.get_n()
+        phi = self.get_phi()
+        dphi = np.zeros((n, n))
+        for i in range(n):
+            for j in range(n):
+                dphi[i, j] = phi[j] - phi[i]
+        return dphi
+
+
+
+
+
+
+
+
+class ChequerboardDefinition(SyncStateDefinition):
+    def __init__(self, system):
+        super(ChequerboardDefinition, self).__init__(system)
+
+
+    def get_phi(self):
+        '''The static phases of all oscillators
+
+           The phase of oscillator 0 is set to 0.
+        '''
+        n = self.sys.g.arr.get_n()
+        phi = np.zeros(n)
+        for i in range(n):
+           phi[i] = ((-1)**(i+1) + 1) * np.pi / 2.0
+        return phi
+
+
+
+
+
+
+
+
+
+
+# #############################################################################
+
+
+class SyncStateFactory(object):
+    def __init__(self, state_def):
+        if isinstance(state_def, ChequerboardDefinition):
+            self.sys = state_def.sys
+            self.state_def = state_def
+        else:
+            raise Exception('State definition type not supported by SyncStateFactory')
+
+
+    def get_phi(self):
+        return self.state_def.get_phi()
+
+
+    def get_dphi_matrix(self):
+        return self.state_def.get_dphi_matrix()
+
+
+    def get_coupling_sum(self, omega, k=1):
+        ''' The sum of all coupling function interacting with oscillator k'''
+        tau = self.sys.g.tau
+        h = self.sys.g.func
+        dphi = self.get_dphi_matrix()[k, :]
+        c = self.sys.g.get_single_site_coupling(k)
+        omega = np.asarray(omega)
+        if omega.size > 1:
+            h_sum = np.zeros(omega.size)
+            for i in range(omega.size):
+                h_sum[i] = np.sum(c * h(-tau * omega[i] + dphi))
+        else:
+            h_sum = np.sum(c * h(-tau * omega + dphi))
+        return h_sum
+
+
+    def get_omega(self, k=1, ns=1000):
+        # Get parameters
+        tau = self.sys.g.tau
+        kc = self.sys.g.k
+        w = self.sys.pll.w
+
+        # Determine min and max values for coupling sum function
+        h_min = self.sys.g.func.min()
+        h_max = self.sys.g.func.max()
+        c_bar = self.sys.g.get_single_site_coupling(k)
+        c_bar_sum = np.sum(c_bar)       # should be 1 for normalized coupling
+        h_sum_min = c_bar_sum * h_min
+        h_sum_max = c_bar_sum * h_max
+
+        # Determine search interval for s
+        s_min = kc * tau * h_sum_min + w * tau
+        s_max = kc * tau * h_sum_max + w * tau
+        s_min = s_min - 2  # add safety margin
+        s_max = s_max + 2
+        if s_min < 0:
+            s_min = 0.0   # exclude negative frequencies
+        s = np.linspace(s_min, s_max, ns)
+
+        # Setup coupling sum function
+        h_sum = lambda x: self.get_coupling_sum(x / tau, k=k)
+
+        # Setup implicit equation for s
+        f = lambda x: kc * tau * h_sum(x) + w * tau - x
+
+        # Find sign changes as you go along curve
+        # Assumes that there are no double sign changes between two values of s
+        # A finer sampling interval can be achieved by increasing ns
+        i_root = get_sign_changes(f(s))
+        if len(i_root) > 0:
+            omega = []
+            for i in range(len(i_root)):
+                # Numerically solve the implicit equation for omega
+                s_tmp = optimize.brentq(f, s[i_root[i]], s[i_root[i] + 1])
+                omega.append(w + kc * h_sum(s_tmp))
+            return omega
+        else:
+            raise Exception('No global synchronization frequency found.')
+
+
+    def get_states(self):
+        omega = self.get_omega()
+        states = []
+        for o in omega:
+            if isinstance(self.state_def, ChequerboardDefinition):
+                states.append(Chequerboard(self.state_def, o))
+        return states
+
+
+
+
+
+
+
+
+
+
+
+# #############################################################################
+
+
+
+
+
+
+
+class SyncState(object):
+    def __init__(self, state_def, omega):
+        self.state_def = state_def
+        self.sys = state_def.sys
+        self.omega = omega
+
+
+    def get_phi(self):
+        return self.state_def.get_phi()
+
+
+    def get_dphi_matrix(self):
+        return self.state_def.get_dphi_matrix()
+
+
+    def get_stability(self, l0=np.array([1.0, 1.0])):
+        funcs = self.get_stability_functions()
+        l = []
+        for f in funcs:
+            l_full = optimize.root(f, l0, tol=1e-14)
+            l_num = l_full.x[0] + 1j * l_full.x[1]
+            l.append(l_num)
+        l = np.array(l)
+        i_max = np.argmax(np.real(l))
+        return l[i_max]
+
+
+    def get_stability_functions(self, k=1):
+        funcs = []
+        b = self.sys.pll.b
+        kc = self.sys.g.k
+        d = self.get_coupling_derivative_matrix()
+        d_sum = np.sum(d[k, :])
+        tau = self.sys.g.tau
+        e, v = self.get_eigensystem()
+        for el in e:
+            alpha = np.real(el)
+            beta = np.imag(el)
+            func_root = lambda l: self._stability_function(l, b, kc, d_sum, tau, alpha, beta)
+            funcs.append(func_root)
+
+        return funcs
+
+
+    def get_coupling_derivative_matrix(self):
+        dphi = self.get_dphi_matrix()
+        h = self.sys.g.func
+        dhdx = h.get_derivative()
+        c = self.sys.g.get_coupling_matrix()
+        return c * dhdx(-self.omega * self.sys.g.tau + dphi)
+
+
+    def get_eigensystem(self, cutoff=1e-6):
+        m = self.get_coupling_derivative_matrix()
+        e, v = np.linalg.eig(m)
+
+        e_tmp = []
+        v_tmp = []
+        for ie in range(len(e)):
+            dv_max = np.max(np.abs(np.diff(v[:, ie])))
+            if dv_max >= cutoff:
+                e_tmp.append(e[ie])
+                v_tmp.append(v[:, ie])
+        e_tmp = np.array(e_tmp)
+        v_tmp = np.transpose(np.array(v_tmp))
+        return e_tmp, v_tmp
+
+
+    @staticmethod
+    def _stability_function(l, b, kc, d_sum, tau, alpha, beta):
+        x = np.zeros(2)
+        zeta = alpha + 1j * beta
+        l_cx = l[0] + 1j * l[1]
+        y = l_cx * (1 + b * l_cx) + kc * d_sum - np.exp(-l_cx * tau) * zeta
+        x[0] = np.real(y)
+        x[1] = np.imag(y)
+        #x[0] = l[0] + b * l[0]**2 - b * l[1]**2 + kc * d_sum - kc * np.exp(-l[0] * tau) * (np.cos(l[1] * tau) * alpha + np.sin(l[1] * tau) * beta)
+        #x[1] = l[1] + 2 * b * l[0] * l[1] - kc * np.exp(-l[0] * tau) * (np.cos(l[1] * tau) * beta - np.sin(l[1] * tau) * alpha)
+        return x
+
+
+
+
+
+
+class Chequerboard(SyncState):
+    def __init__(self, state_def, omega):
+        if isinstance(state_def, ChequerboardDefinition):
+            super(Chequerboard, self).__init__(state_def, omega)
+        else:
+            raise Exception('Non-compatible StateDefinition')
+
+
+
+

GlobFreq_LinStab/synctools_interface.py
@@ -205,10 +205,10 @@ class FlatStateList(object):
            State or list of TwistStates
                state or list of states that should be added
         '''
-        if type(s) is st.State:
+        if isinstance(s, st.SyncState):
             self.states.append(s)
             self.n = len(self.states)
-        elif type(s) is list:
+        elif isinstance(s, list):
             for el in s:
                 self.states.append(el)
             self.n = len(self.states)
@@ -350,10 +350,14 @@ class FlatStateList(object):
     def get_tsim(self):
         '''Returns an array of simulation time'''
         if self.n > 0:
-            x = self.tsim * np.ones(self.n)
+            x = np.zeros(self.n)
+            for i in range(self.n):
+                re_lambda = np.real(self.states[i].get_stability())
+                x[i] = 25.0 / np.abs(re_lambda)
             return x
         else:
             return None
+
 
     def get_nx(self):
         if self.n > 0:

GlobFreq_LinStab/test_interface_01.py
@@ -1,15 +1,16 @@
 import numpy as np
 
 import synctools_interface
-
+import synctools2 as st
 
 n = 3
 topology = 'ring'
+topology = 'chain'
 w = 2 * np.pi
-k = np.pi / 2.0
+kc = np.pi / 2.0
 wc = w
 h = 'triang'
-m = 1
+m = 0
 tau = np.linspace(0, 1.7, 5)
 
 ny = 0
@@ -19,6 +20,7 @@ my = 0
 c = 0
 
 
-sf = synctools_interface.SweepFactory(n, ny, nx, w, k, tau, h, wc, m, mx, my, topology, c, tsim=0.0, isRadians=True)
+sf = synctools_interface.SweepFactory(n, ny, nx, w, kc, tau, h, wc, m, mx, my, topology, c, tsim=0.0, isRadians=True)
 fsl = sf.sweep()
 para_mat = fsl.get_parameter_matrix()
+s = fsl.states[2]